693100f8fa1a7174d56646c2e79895228e468e25
sebilaz99 <lazeasebastian@gmail.com>
2021-12-04 14:17:23 +0200

    -Extras Fragment: implemented Tips Layout BottomNavigationView -few assets + layouts -RecyclerAdapter for EXTRAS

---
+++ app/src/main/java/com/example/filmaniac/RecyclerAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.filmaniac
+
+import android.app.Dialog
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    val tipNumbers = arrayOf("Tip #1", "Tip #2", "Tip #3")
+    val tipTexts = arrayOf(
+        "What should I begin with?",
+        "Where can I modify the salary and the salary percentage",
+        "Think about it later"
+    )
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.tip_item, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        holder.number.text = tipNumbers[position]
+        holder.text.text = tipTexts[position]
+
+    }
+
+    override fun getItemCount(): Int {
+        return tipNumbers.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var number = itemView.findViewById<TextView>(R.id.tipNumber)
+        var text = itemView.findViewById<TextView>(R.id.tipText)
+
+        lateinit var dialog: Dialog
+
+        init {
+            itemView.setOnClickListener {
+                when (position) {
+                    0 -> {
+                        dialog = Dialog(itemView.context)
+                        dialog.setContentView(R.layout.custom_popup_0)
+                        dialog.show()
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
