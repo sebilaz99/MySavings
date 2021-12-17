5d823f2d17c79f33bc4e72d0868c72a6fd7bc3b7
sebilaz <lazeasebastian@gmail.com>
2021-12-17 13:04:48 +0200

    ResetPassword Activity ui + backend

---
+++ app/src/main/java/com/example/filmaniac/ui/ResetPassword.kt
@@ -0,0 +1,50 @@
+package com.example.filmaniac.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.filmaniac.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+
+class ResetPassword : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+
+        val backBtn = findViewById<Button>(R.id.backToLoginButton)
+        val emailEditTxt = findViewById<EditText>(R.id.emailEditText)
+        val submitBtn = findViewById<Button>(R.id.submitButton)
+
+        auth = FirebaseAuth.getInstance()
+
+        val email = emailEditTxt.text.toString().trim()
+
+        submitBtn.setOnClickListener {
+            auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Please check your e-mail", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Something went wrong! Please try again.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+
+        backBtn.setOnClickListener {
+            val goBackIntent = Intent(this, Login::class.java)
+            startActivity(goBackIntent)
+        }
+
+    }
+}
